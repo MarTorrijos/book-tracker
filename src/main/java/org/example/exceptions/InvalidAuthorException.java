0a1336ae77b26a94b333bd780b02313cfb18af80
mar <martorrijos@protonmail.com>
2025-08-02 09:14:28 +0200

    chore: deleted unused exceptions

--- src/main/java/org/example/exceptions/InvalidAuthorException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exceptions;
-
-public class InvalidAuthorException extends RuntimeException {
-    public InvalidAuthorException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/example/exceptions/InvalidBookException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exceptions;
-
-public class InvalidBookException extends RuntimeException {
-    public InvalidBookException(String message) {
-        super(message);
-    }
-}
