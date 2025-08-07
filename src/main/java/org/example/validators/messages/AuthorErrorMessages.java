208e1c0d8b92ab410927e9917adcea944dc7bf1b
mar <martorrijos@protonmail.com>
2025-08-07 10:31:09 +0200

    feat: validation error messages

---
+++ src/main/java/org/example/validators/messages/AuthorErrorMessages.java
@@ -0,0 +1,15 @@
+package org.example.validators.messages;
+
+public final class AuthorErrorMessages {
+
+    private AuthorErrorMessages() {
+        // Created to prevent instantiation
+    }
+
+    public static final String AUTHOR_REQUIRED =
+        "An author is required";
+
+    public static final String AUTHOR_TOO_LONG =
+            "Author name is too long. It must be less than 300 characters";
+
+}
