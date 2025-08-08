fbc4fc6112e0391634576dde00bba4c978d3394a
mar <martorrijos@protonmail.com>
2025-08-08 11:56:13 +0200

    fix(GenresValidator): add invalid genre name to exception message

--- src/main/java/org/example/validators/implementations/book/GenresValidator.java
+++ src/main/java/org/example/validators/implementations/book/GenresValidator.java
@@ -12,7 +12,7 @@ public class GenresValidator implements FieldValidator<String[]> {
         if (genres != null) {
             for (String genre : genres) {
                 if (genre.length() > 50) {
-                    throw new FieldValidationException(GENRE_TOO_LONG);
+                    throw new FieldValidationException(GENRE_TOO_LONG + ": " + genre);
                 }
             }
         }
