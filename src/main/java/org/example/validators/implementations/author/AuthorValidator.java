c4a2733bd4776f548d90d81c3ba7ff0fab7dfe03
mar <martorrijos@protonmail.com>
2025-08-08 11:54:53 +0200

    fix(AuthorValidator): handle empty author name in validation

--- src/main/java/org/example/validators/implementations/author/AuthorValidator.java
+++ src/main/java/org/example/validators/implementations/author/AuthorValidator.java
@@ -9,7 +9,7 @@ public class AuthorValidator implements FieldValidator<String> {
 
     @Override
     public void validate(String name) {
-        if (name == null) {
+        if (name == null || name.trim().isEmpty()) {
             throw new FieldValidationException(AUTHOR_NAME_REQUIRED);
         } else if (name.length() > 300) {
             throw new FieldValidationException(AUTHOR_NAME_TOO_LONG);
