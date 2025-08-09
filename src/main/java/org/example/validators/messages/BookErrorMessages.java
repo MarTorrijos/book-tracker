0e956d72d549f8a8ed822ae959073e8707c51ee1
mar <martorrijos@protonmail.com>
2025-08-09 12:33:35 +0200

    feat: add page count too high message

--- src/main/java/org/example/validators/messages/BookErrorMessages.java
+++ src/main/java/org/example/validators/messages/BookErrorMessages.java
@@ -12,6 +12,9 @@ public final class BookErrorMessages {
     public static final String PAGES_NOT_POSITIVE =
             "Page count must be a positive number";
 
+    public static final String PAGE_COUNT_TOO_HIGH =
+            "Page count is too high. It can't be greater than 2000";
+
     public static final String PUBLISHED_IN_NOT_VALID =
             "Published year must be valid (between 0 and %d)";
 
