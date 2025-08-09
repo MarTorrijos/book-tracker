7737a3176d18caef09513d2d2330dd530b0884fd
mar <martorrijos@protonmail.com>
2025-08-09 12:32:26 +0200

    fix(PagesValidator): improve null check in loop and rename message variable

--- src/main/java/org/example/validators/implementations/book/PagesValidator.java
+++ src/main/java/org/example/validators/implementations/book/PagesValidator.java
@@ -4,13 +4,18 @@ import org.example.exceptions.FieldValidationException;
 import org.example.validators.FieldValidator;
 
 import static org.example.validators.messages.BookErrorMessages.PAGES_NOT_POSITIVE;
+import static org.example.validators.messages.BookErrorMessages.PAGE_COUNT_TOO_HIGH;
 
 public class PagesValidator implements FieldValidator<Integer> {
 
     @Override
     public void validate(Integer pages) {
-        if (pages != null && pages <= 0) {
-            throw new FieldValidationException(PAGES_NOT_POSITIVE);
+        if (pages != null) {
+            if (pages < 0) {
+                throw new FieldValidationException(PAGES_NOT_POSITIVE);
+            } else if (pages > 2000) {
+                throw new FieldValidationException(PAGE_COUNT_TOO_HIGH);
+            }
         }
     }
 
