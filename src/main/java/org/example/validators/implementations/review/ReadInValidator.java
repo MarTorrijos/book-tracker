4b0bd44c2e932abc3a6e529aa7b8745c59ecab81
mar <martorrijos@protonmail.com>
2025-08-11 07:26:26 +0200

    fix: small updates to ReadInValidator

--- src/main/java/org/example/validators/implementations/review/ReadInValidator.java
+++ src/main/java/org/example/validators/implementations/review/ReadInValidator.java
@@ -3,14 +3,22 @@ package org.example.validators.implementations.review;
 import org.example.exceptions.FieldValidationException;
 import org.example.validators.FieldValidator;
 
+import java.time.Year;
+
 import static org.example.validators.messages.ReviewErrorMessages.INCORRECT_YEAR_READ_IN;
 
 public class ReadInValidator implements FieldValidator<Integer> {
 
+    private final int CURRENT_YEAR = Year.now().getValue();
+
     @Override
     public void validate(Integer year) {
-        if (year < 1989 || year > 2025) {
-            throw new FieldValidationException(INCORRECT_YEAR_READ_IN);
+        int USER_BORN_IN = 1989;
+
+        if (year != null) {
+            if (year < USER_BORN_IN || year > CURRENT_YEAR) {
+                throw new FieldValidationException(INCORRECT_YEAR_READ_IN);
+            }
         }
     }
 
