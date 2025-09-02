d0716166d5fdd549d0b0d121b46d7c76ada2ac84
mar <martorrijos@protonmail.com>
2025-09-02 11:27:35 +0200

    update BookCrudService

--- src/main/java/org/example/service/book/BookCrudService.java
+++ src/main/java/org/example/service/book/BookCrudService.java
@@ -9,17 +9,15 @@ import org.example.entities.Author;
 import org.example.entities.Book;
 import org.example.exceptions.BookNotFoundException;
 
-import java.util.List;
-
 import static org.example.log.BookLogMessages.*;
 
-public class BookService {
+public class BookCrudService {
 
     private final BookDao bookDao;
     private final AuthorDao authorDao;
-    private static final Logger logger = LogManager.getLogger(BookService.class.getName());
+    private static final Logger logger = LogManager.getLogger(BookCrudService.class.getName());
 
-    public BookService(BookDao bookDao, AuthorDao authorDao) {
+    public BookCrudService(BookDao bookDao, AuthorDao authorDao) {
         this.bookDao = bookDao;
         this.authorDao = authorDao;
     }
@@ -57,43 +55,6 @@ public class BookService {
         }
         bookDao.delete(id);
         logger.info(DELETE_SUCCESS_LOG, id.toHexString());
-    }
-
-    public List<Book> findAll() {
-        return bookDao.findAllBooks();
-    }
-
-    public Book findById(ObjectId id) {
-        Book book = bookDao.findBookById(id);
-
-        if (book == null) {
-            logger.warn(FIND_BY_ID_FAIL_LOG, id.toHexString());
-            throw new BookNotFoundException("Book not found");
-        }
-        logger.info(FIND_BY_ID_SUCCESS_LOG, id.toHexString());
-        return book;
-    }
-
-    public Book findByTitle(String title) {
-        Book book = bookDao.findBookByTitle(title);
-
-        if (book == null) {
-            logger.warn(FIND_BY_TITLE_FAIL_LOG, title);
-            throw new BookNotFoundException("Book not found");
-        }
-        logger.info(FIND_BY_TITLE_SUCCESS_LOG, title);
-        return book;
-    }
-
-    public List<Book> findByAuthor(String author) {
-        List<Book> books = bookDao.findBookByAuthor(author);
-
-        if (books.isEmpty()) {
-            logger.warn(FIND_BY_AUTHOR_FAIL, author);
-            throw new BookNotFoundException("No books found for that author");
-        }
-        logger.info(FIND_BY_AUTHOR_SUCCESS, author);
-        return books;
     }
 
 }
