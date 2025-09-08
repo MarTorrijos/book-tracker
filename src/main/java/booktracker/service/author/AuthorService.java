f965a0805e07cf760949229439e84e80fa5529f7
mar <martorrijos@protonmail.com>
2025-09-08 08:51:13 +0200

    feat: create AuthorService

--- src/main/java/booktracker/service/author/AuthorService.java
+++ src/main/java/booktracker/service/author/AuthorService.java
@@ -1,9 +1,27 @@
 package booktracker.service.author;
 
+import booktracker.dao.author.AuthorDao;
+import booktracker.entities.Author;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class AuthorService {
 
-    // Add/update authors
-    // Search existing authors (prevent duplicates)
-    // Normalize data
+    private final AuthorDao authorDao;
+    private static final Logger logger = LogManager.getLogger(AuthorService.class.getName());
+
+    public AuthorService(AuthorDao authorDao) {
+        this.authorDao = authorDao;
+    }
+
+    public Author findByName(String name) {
+        return authorDao.findByName(name);
+    }
+
+    public Author save(Author author) {
+        return authorDao.insert(author);
+    }
+
+    // update authors
 
 }
