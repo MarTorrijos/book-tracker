96183ea11ec4ee67da1f8fe524b79411a4d5f9d8
mar <martorrijos@protonmail.com>
2025-09-09 08:28:11 +0200

    feat: create BookQueryServiceTest

---
+++ src/test/java/booktracker/service/BookQueryServiceTest.java
@@ -0,0 +1,102 @@
+package booktracker.service;
+
+import booktracker.exceptions.BookNotFoundException;
+import booktracker.testdata.DataProvider;
+import org.bson.types.ObjectId;
+import booktracker.dao.book.BookDao;
+import booktracker.entities.Book;
+import booktracker.service.book.BookQueryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class BookQueryServiceTest {
+
+    @Mock
+    private BookDao bookDaoMock;
+
+    @InjectMocks
+    private BookQueryService service;
+
+    private ObjectId id;
+    private Book book;
+    private String title;
+    private List<Book> bookList;
+
+    @BeforeEach
+    void setUp() {
+        book = DataProvider.validBook();
+        title = book.getTitle();
+        id = book.getId();
+        bookList = DataProvider.bookListMock();
+    }
+
+    @Test
+    void findAll_Success() {
+        when(bookDaoMock.findAllBooks()).thenReturn(bookList);
+        List<Book> result = service.findAll();
+
+        assertNotNull(result);
+        assertFalse(result.isEmpty());
+        verify(bookDaoMock).findAllBooks();
+    }
+
+    @Test
+    void findById_Success() {
+        when(bookDaoMock.findBookById(id)).thenReturn(book);
+        Book foundBook = service.findById(id);
+
+        assertNotNull(foundBook);
+        assertEquals("Dune", foundBook.getTitle());
+        verify(bookDaoMock).findBookById(id);
+    }
+
+    @Test
+    void findById_NotFound() {
+        ObjectId wrongId = new ObjectId();
+        when(bookDaoMock.findBookById(wrongId)).thenReturn(null);
+
+        assertThrows(BookNotFoundException.class, () -> service.findById(wrongId));
+        verify(bookDaoMock).findBookById(wrongId);
+    }
+
+    @Test
+    void findByTitle_Success() {
+        when(bookDaoMock.findBookByTitle(title)).thenReturn(book);
+
+        assertDoesNotThrow(() -> service.findByTitle(title));
+        verify(bookDaoMock).findBookByTitle(title);
+    }
+
+    @Test
+    void findByTitle_NotFound() {
+        when(bookDaoMock.findBookByTitle(title)).thenReturn(null);
+
+        assertThrows(BookNotFoundException.class, () -> service.findByTitle(title));
+        verify(bookDaoMock).findBookByTitle(title);
+    }
+
+    @Test
+    void findByAuthor_Success() {
+        String authorList = "Ursula K. Le Guin";
+        when(bookDaoMock.findBookByAuthor(authorList)).thenReturn(bookList);
+
+        List<Book> result = service.findByAuthor(authorList);
+
+        assertFalse(result.isEmpty());
+        assertTrue(result.stream().
+                allMatch(b -> authorList.equals(b.getAuthor().getName())));
+        verify(bookDaoMock).findBookByAuthor(authorList);
+    }
+
+}
