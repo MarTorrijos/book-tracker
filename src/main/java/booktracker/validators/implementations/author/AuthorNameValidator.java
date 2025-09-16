bfa94d677d394848db9c54f6990412c158dcca55
mar <martorrijos@protonmail.com>
2025-09-16 07:14:49 +0200

    feat: reduce max author name length to 100 characters

--- src/main/java/booktracker/validators/implementations/author/AuthorNameValidator.java
+++ src/main/java/booktracker/validators/implementations/author/AuthorNameValidator.java
@@ -3,15 +3,16 @@ package booktracker.validators.implementations.author;
 import booktracker.exceptions.FieldValidationException;
 import booktracker.validators.FieldValidator;
 
-import static booktracker.validators.messages.AuthorErrorMessages.*;
+import static booktracker.validators.messages.AuthorErrorMessages.AUTHOR_NAME_REQUIRED;
+import static booktracker.validators.messages.AuthorErrorMessages.AUTHOR_NAME_TOO_LONG;
 
-public class AuthorValidator implements FieldValidator<String> {
+public class AuthorNameValidator implements FieldValidator<String> {
 
     @Override
     public void validate(String name) {
         if (name == null || name.trim().isEmpty()) {
             throw new FieldValidationException(AUTHOR_NAME_REQUIRED);
-        } else if (name.length() > 300) {
+        } else if (name.length() > 100) {
             throw new FieldValidationException(AUTHOR_NAME_TOO_LONG);
         }
     }
