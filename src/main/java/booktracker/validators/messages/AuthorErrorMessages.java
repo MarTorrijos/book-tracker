66e5540fa1c05cecefd294c43845c62491aa8a5e
mar <martorrijos@protonmail.com>
2025-09-16 07:13:19 +0200

    feat: add new author error messages for validation

--- src/main/java/booktracker/validators/messages/AuthorErrorMessages.java
+++ src/main/java/booktracker/validators/messages/AuthorErrorMessages.java
@@ -7,9 +7,15 @@ public final class AuthorErrorMessages {
     }
 
     public static final String AUTHOR_NAME_REQUIRED =
-        "An author name is required";
+            "An author name is required";
 
     public static final String AUTHOR_NAME_TOO_LONG =
-            "Author name is too long. It must be less than 300 characters";
+            "Author name is too long. It must be less than 100 characters";
+
+    public static final String AUTHOR_SURNAME_REQUIRED =
+            "An author surname is required";
+
+    public static final String AUTHOR_SURNAME_TOO_LONG =
+            "Author surname is too long. It must be less than 200 characters";
 
 }
