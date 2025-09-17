af8637b6f099149ff955565747c2624c339037fc
mar <martorrijos@protonmail.com>
2025-09-17 07:45:56 +0200

    feat: create BookStatsService

--- src/main/java/booktracker/service/stats/BookStatsService.java
+++ src/main/java/booktracker/service/stats/BookStatsService.java
@@ -1,32 +1,31 @@
 package booktracker.service.stats;
 
-import booktracker.dao.book.BookDao;
-import booktracker.entities.Book;
+import booktracker.dao.stats.BookStatsDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static booktracker.log.BookStatsLogMessages.COUNT_BOOKS_READ_IN_YEAR;
 
 public class BookStatsService {
 
-    private final BookDao bookDao;
+    private final BookStatsDao bookStatsDao;
+    private static final Logger logger = LogManager.getLogger(BookStatsService.class.getName());
 
-    public BookStatsService(BookDao bookDao) {
-        this.bookDao = bookDao;
+    public BookStatsService(BookStatsDao bookStatsDao) {
+        this.bookStatsDao = bookStatsDao;
     }
 
     public int countBooksReadThisYear() {
-
+        return bookStatsDao.countBooksReadThisYear();
     }
 
     public int countBooksReadInGivenYear(int year) {
-
+        logger.debug(COUNT_BOOKS_READ_IN_YEAR, year);
+        return bookStatsDao.countBooksReadInGivenYear(year);
     }
 
     public int countTotalBooksRead() {
-
-    }
-
-    // más?
-
-    private Integer getReadYear(Book book) {
-        return book.getReview() != null ? book.getReview().getReadIn() : null;
+        return bookStatsDao.countTotalBooksRead();
     }
 
 }
