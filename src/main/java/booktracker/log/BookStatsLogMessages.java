b8961ef410c3504af49432dc400dd51e69a6727d
mar <martorrijos@protonmail.com>
2025-09-17 07:45:31 +0200

    feat: create BookStatsLogMessages

---
+++ src/main/java/booktracker/log/BookStatsLogMessages.java
@@ -0,0 +1,12 @@
+package booktracker.log;
+
+public final class BookStatsLogMessages {
+
+    public BookStatsLogMessages() {
+        // Created to prevent instantiation
+    }
+
+    public static final String COUNT_BOOKS_READ_IN_YEAR =
+            "Counting books read in year: {}";
+
+}
