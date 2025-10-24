b0e4d2d7a0ac03f9c756546cc06673c1b121354a
mar <martorrijos@protonmail.com>
2025-10-24 10:54:24 +0200

    feat(log): create AuthorStatsLogMessages

---
+++ src/main/java/booktracker/log/AuthorStatsLogMessages.java
@@ -0,0 +1,11 @@
+package booktracker.log;
+
+public class AuthorStatsLogMessages {
+
+    public static final String MOST_READ_AUTHOR_FAIL_LOG =
+            "No authors found. Couldn't show most read author";
+
+    public static final String BEST_RATED_AUTHOR_FAIL_LOG =
+            "No authors found. Couldn't show author with best rated books";
+
+}
