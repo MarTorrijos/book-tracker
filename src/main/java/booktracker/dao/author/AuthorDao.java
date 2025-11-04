fe8fd5866df64f4e36ef26da9b27f6c88970037f
MarT <gitmt@proton.me>
2025-11-04 15:33:48 +0100

    feat(dao): add new functionalities to AuthorDao

--- src/main/java/booktracker/dao/author/AuthorDao.java
+++ src/main/java/booktracker/dao/author/AuthorDao.java
@@ -1,10 +1,17 @@
 package booktracker.dao.author;
 
 import booktracker.entities.Author;
+import org.bson.types.ObjectId;
+
+import java.util.List;
 
 public interface AuthorDao {
 
-    Author findByName(String name);
-    Author insert(Author author);
+    void insert(Author author);
+    void update(ObjectId id, Author author);
+
+    Author findByFullName(String name, String surname);
+    List<Author> findBySurname(String surname);
+    boolean existsByFullName(String name, String surname);
 
 }
